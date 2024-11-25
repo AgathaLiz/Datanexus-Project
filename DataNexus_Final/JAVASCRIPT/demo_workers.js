418632f0ab70e39b81eab31833c612abde037465
Julia-Rodrigues-da-Silva <julia.rodrigues1008s@gmail.com>
2024-11-24 21:31:13 -0300

    Delete DataNexus_Final directory

--- DataNexus_Final/JAVASCRIPT/demo_workers.js
+++
@@ -1,9 +0,0 @@
-let i = 0; /*Váriável para iniciar zerado*/
-
-function timedCount(){
-  i++; /*Sempre aumenta de um em um*/ 
-  postMessage(i); /*Aqui ele manda para a página*/
-  setTimeout("timedCount()", 1000); /*Aumenta o i a cada segundo */
-}
-
-timedCount(); /*Chama a função*/
